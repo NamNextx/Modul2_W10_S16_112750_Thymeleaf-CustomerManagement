4257419e488949b63960f679d218dd5757b6dc63
Namnextx <phuongnam.nextx@gmail.com>
2019-04-23 20:57:58 +0700

    2. - add class Customer - Service:  + Customer service  + ICustomer service

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -1,10 +1,10 @@
-package com.codegym;
+package com.codegym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controller {
+public class CustomerController {
     @GetMapping("/home")
     public String helloNam(){
         return "index";
