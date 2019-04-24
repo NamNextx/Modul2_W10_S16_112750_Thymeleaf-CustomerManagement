f0d147dcff2daa2c2d57212e8e52660531edcf72
Namnextx <phuongnam.nextx@gmail.com>
2019-04-24 07:06:30 +0700

    add file layout.html to save footer and header add file: service and interface service

--- src/main/java/com/codegym/service/CustomerServiceImpl.java
+++ src/main/java/com/codegym/service/CustomerServiceImpl.java
@@ -1,4 +1,42 @@
 package com.codegym.service;
 
-public class CustomerServiceImpl {
+import com.codegym.model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServiceImpl implements CustomerService {
+    private static Map<Integer,Customer> customerMap;
+
+    static {
+        customerMap=new HashMap<>();
+        customerMap.put(1,new Customer(1,"nam","bob","hoang"));
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerMap.put(customer.getId(),customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customerMap.get(id);
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        customerMap.put(id,customer);
+    }
+
+    @Override
+    public void remove(int id) {
+        customerMap.remove(id);
+    }
 }
