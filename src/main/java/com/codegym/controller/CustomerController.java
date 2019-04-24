6f73d726230488631d0d40c9588b6a36d8b38e48
Namnextx <phuongnam.nextx@gmail.com>
2019-04-24 10:13:40 +0700

    add delete html file

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -6,6 +6,9 @@ import com.codegym.service.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerController {
@@ -22,6 +25,33 @@ public class CustomerController {
         model.addAttribute("customer",new Customer());
         return "create";
     }
+
+    @PostMapping("/customer/save")
+    public String save(Customer customer, RedirectAttributes redirect){
+        customer.setId((int)(Math.random())*1000);
+        customerService.save(customer);
+        redirect.addFlashAttribute("success","Saved customer successfully!");
+        return "redirect:/"; // comeback to " url "/" as jum to index.html
+    }
+
+    @GetMapping("/customer/{id}/edit")
+    public String edit (@PathVariable int id, Model model){
+        model.addAttribute("customer",customerService.findById(id));
+        return "edit";
+    }
+    @PostMapping("customer/update")
+    public String update(Customer customer,RedirectAttributes redirect){
+        customerService.update(customer.getId(),customer);
+        redirect.addFlashAttribute("success","Modify customer successfully!");
+        return "redirect:/";
+    }
+
+    @GetMapping("/customer/{id}/delete")
+    public String delete(@PathVariable int id,Model model){
+        model.addAttribute("customer",customerService.findById(id));
+        return "delete";
+    }
+
 
 }
 
